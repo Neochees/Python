300301f06a8004a0592d9c10504296c3150bda49
Neochees <113420296+Neochees@users.noreply.github.com>
2023-10-02 11:41:05 +0200

    done

--- test2.py
+++ test2.py
@@ -1,8 +1,8 @@
 import pyautogui
 
-pyautogui.write('Watch this :3', interval = 0.1)
-pyautogui.hotkey('alt', 'tab')
-pyautogui.hotkey('ctrl', 't') 
-pyautogui.write('https://www.youtube.com/watch?v=dQw4w9WgXcQ', interval = 0)
-pyautogui.hotkey('enter') 
+pyautogui.write('Watch this :3', interval = 0.1) # hier typed hij wat text
+pyautogui.hotkey('alt', 'tab') # nu veranderd hij van tabblad
+pyautogui.hotkey('ctrl', 't') # dan voegt hij hier een nieuw tabblad aan
+pyautogui.write('https://www.youtube.com/watch?v=dQw4w9WgXcQ', interval = 0) # en plaats hij de link
+pyautogui.hotkey('enter') # uiteindelijk zoekt hij de geplaatste link op
 
