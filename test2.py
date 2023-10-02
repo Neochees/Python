fbb4df9ea1a2e2f976decf997f22b549a91f3685
Neochees <113420296+Neochees@users.noreply.github.com>
2023-10-02 11:29:45 +0200

    commit 2

---
+++ test2.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+pyautogui.write('Watch this :3', interval = 0.1)
+pyautogui.hotkey('alt', 'tab')
+pyautogui.hotkey('ctrl', 't') 
+pyautogui.write('https://www.youtube.com/watch?v=dQw4w9WgXcQ', interval = 0)
+pyautogui.hotkey('enter') 
+
